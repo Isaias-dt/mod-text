89b55f3e4728b41f7f617d49267c3090b899893d
Isaias-dt <isaias-dt@hotmail.com>
2021-06-04 07:43:14 -0300

    action copy e paste finished

--- js.js
+++ js.js
@@ -1,30 +1,38 @@
 (function(win, doc) {
   'use strict';
 
-  var txtInput = doc.querySelector('[data-js="txtInput"]');
-  var btnPaste = doc.querySelector('[data-js="btnPaste"]');
-  var btnConvert = doc.querySelector('[data-js="btnConvert"]');
-  var txtOutput = doc.querySelector('[data-js="txtOutput"]');
-  var btnCopy = doc.querySelector('[data-js="btnCopy"]');
+  var txtInput = doc.querySelector('[data-js=txtInput]');
+  var btnPaste = doc.querySelector('[data-js=btnPaste]');
+  var btnConvert = doc.querySelector('[data-js=btnConvert]');
+  var txtOutput = doc.querySelector('[data-js=txtOutput]');
+  var btnCopy = doc.querySelector('[data-js=btnCopy]');
+  var btnLimpar = doc.querySelector('[data-js=btnLimpar]');
 
-  btnPaste.addEventListener('click', function(evt) {
-
-  }, false);
-
+  btnLimpar.addEventListener('click', toClearCamp, false);
   btnConvert.addEventListener('click', txtUpperCase, false);
+  btnPaste.addEventListener('click', toPasteText, false);
+  btnCopy.addEventListener('click', toCopyText, false);
 
-  btnCopy.addEventListener('click', function(evt) {
-      
-  }, false);
-
-  function pasteText(evt) {
+  function toPasteText(evt) {
     evt.preventDefault();
+    win.navigator.clipboard.readText().then(
+    clipText => txtInput.value = clipText);
   }
 
   function txtUpperCase(evt) {
     evt.preventDefault();
     var text = txtInput.value;
     txtOutput.innerHTML = text.toUpperCase();
+  }
+
+  function toClearCamp(evt) {
+    evt.preventDefault();
+    txtInput.value = '';
+  }
+
+  function toCopyText(evt) {
+    evt.preventDefault();
+    win.navigator.clipboard.writeText(txtOutput.textContent);
   }
 
 })(window, document);
